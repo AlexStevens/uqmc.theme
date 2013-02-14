1ce74bf6aca2caff234b751bf40475f1fba72d2b
Alex Stevens <alexander.stevens@uqconnect.edu.au>
2013-02-14 19:58:26 +1000

    Site structure completed, need css styling

---
+++ src/uqmc/theme/viewlets/uqmc_main.py
@@ -0,0 +1,16 @@
+from five import grok
+from zope.interface import Interface
+from plone.app.layout.viewlets.interfaces import IPortalFooter
+
+from uqmc.theme.interfaces import IThemeSpecific
+
+
+grok.templatedir('templates')
+grok.layer(IThemeSpecific)
+
+
+class FooterViewlet(grok.Viewlet):
+    grok.name('uqmc.footer')
+    grok.context(Interface)
+    grok.viewletmanager(IPortalFooter)
+    grok.order(1)
