69116fddb78f36db950d650826564a5e53c2df3e
Alexander Stevens <alex@mooball.net>
2013-02-15 17:58:50 +1000

    Replaced main template, added new viewlet manager, structure finally there

--- src/uqmc/theme/interfaces.py
+++ src/uqmc/theme/interfaces.py
@@ -1,8 +1,13 @@
 from zope.interface import Interface
+from zope.viewlet.interfaces import IViewletManager
 
 class IThemeSpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer.
        If you need to register a viewlet only for the
        "NVP Theme" theme, this interface must be its layer
        (in skin/viewlets/configure.zcml).
+    """
+
+class IBetweenContent(IViewletManager):
+    """ Viewlet manager for in between plone.portaltop and plone.abovecontent
     """
