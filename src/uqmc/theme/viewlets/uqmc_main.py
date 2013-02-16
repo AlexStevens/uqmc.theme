5bc9ddcbf69ceb91500b45866817f324da710ff2
Alex Stevens <alexander.stevens@uqconnect.edu.au>
2013-02-17 01:10:57 +1000

    Updated footer viewlet to show latest news item content

--- src/uqmc/theme/viewlets/uqmc_main.py
+++ src/uqmc/theme/viewlets/uqmc_main.py
@@ -1,5 +1,6 @@
 from five import grok
 from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets.interfaces import IPortalFooter
 
 from uqmc.theme.interfaces import IThemeSpecific
@@ -14,3 +15,11 @@ class FooterViewlet(grok.Viewlet):
     grok.context(Interface)
     grok.viewletmanager(IPortalFooter)
     grok.order(1)
+
+    def get_newest_newsitem(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        news_items = catalog({'portal_type': 'News Item',
+                'sort_on': 'effective', 'sort_order': 'descending' })
+
+        if len(news_items):
+            return news_items[0]
